107883caabae1ba3372dbdddf8cced2ed352fe5c
bin <bin@upet.fun>
2021-07-20 12:01:43 +0800

    remove useless menu

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -5,7 +5,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">项目</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <!-- <ol class="breadcrumb float-sm-right"> -->
@@ -18,9 +18,22 @@
     </div>
     <!-- /.content-header -->
     <section class="content">
-      <!-- <Project ref='project'></Project> -->
+      <Project ref='project'></Project>
     </section>
     <!-- /.content -->
   </div>
 </template>
 
+<script>
+import Project from '../components/Project.vue'
+
+export default{
+  name: 'Dashboard',
+  components: {
+    Project
+  },
+  mounted(){
+    this.$refs['project'].loadProjectList()
+  }
+}
+</script>
